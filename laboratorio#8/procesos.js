fe97f8cd80c5086a8bfa7a58cc8ce6c6c5cafbba
luis gomez <luis.gomez13@utp.ac.pa>
2022-10-14 08:36:20 -0500

    Se agrega JS

--- laboratorio#8/procesos.js
+++ laboratorio#8/procesos.js
@@ -1,1 +1,23 @@
-function calculos = document.getElementById("datos").ariaValueMax
+function order(cadena) {
+    var mensaje = document.getElementById(cadena).value;
+    var cadena = mensaje.split("");
+    var contador = {};
+    cadena.forEach(function (valor) {
+      if (contador[valor] === undefined) {
+        contador[valor] = 1;
+      } else {
+        contador[valor] += 1;
+      }
+    });
+    document.getElementById("resultado").innerHTML = "<ul>";
+
+    for (var clave in contador) {
+      document.getElementById("resultado").innerHTML +=
+        "<li>" + clave + ":" + contador[clave] + "</li>";
+    }
+    document.getElementById("resultado").innerHTML += "<ul>";
+  }
+  function limpiar(cadena) {
+    document.getElementById(cadena.value) = "";
+    document.getElementById("resultado").innerHTML = "";
+  }
